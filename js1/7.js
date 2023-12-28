4ada3e72e21b0fe1ca6e0fa6ce299e272489d88e
Yu Zhou Zhang <yuzspanish@gmail.com>
2023-12-28 14:46:06 +0000

    solves problem 7

--- js1/7.js
+++ js1/7.js
@@ -9,9 +9,15 @@
  */
 
 const solution = (inp, letter, i = 0) => {
-  return true
-}
+  if (i >= inp.length) {
+    return false;
+  }
+  if (inp[i] === letter) {
+    return true;
+  }
+  return solution(inp, letter, i + 1);
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
